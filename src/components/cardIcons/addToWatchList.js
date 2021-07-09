35607964f3773ca73a3cd4b1beac8be34758f9bb
Sean-Crowley <seancrowley033@gmail.com>
2021-07-09 14:52:01 +0100

    New button for upcoming movies page

---
+++ src/components/cardIcons/addToWatchList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PlaylistAddIcon from "@material-ui/icons/PlaylistAdd";
+
+const WriteReviewIcon = ({ movie }) => {
+  return (
+    <PlaylistAddIcon color="primary" fontSize="large" />
+  );
+};
+
+export default  WriteReviewIcon;

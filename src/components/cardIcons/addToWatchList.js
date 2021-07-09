b998b3da9bac417ab9da93531f02ab614f0f5054
Sean-Crowley <seancrowley033@gmail.com>
2021-07-09 15:19:34 +0100

    Tagging upcoming movies as must-watch.

--- src/components/cardIcons/addToWatchList.js
+++ src/components/cardIcons/addToWatchList.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MoviesContext } from "../../contexts/moviesContext";
+import IconButton from "@material-ui/core/IconButton";
 import PlaylistAddIcon from "@material-ui/icons/PlaylistAdd";
 
-const WriteReviewIcon = ({ movie }) => {
+const AddToWatchListIcon = ({ movie }) => {
+  const context = useContext(MoviesContext);
+
+  const handleAddToMustWatchList = (e) => {
+    e.preventDefault();
+    context.addToMustWatch(movie);
+  };
   return (
-    <PlaylistAddIcon color="primary" fontSize="large" />
+    <IconButton
+      aria-label="add to favorites"
+      onClick={handleAddToMustWatchList}
+    >
+      <PlaylistAddIcon color="primary" fontSize="large" />
+    </IconButton>
   );
 };
 
-export default  WriteReviewIcon;
+export default AddToWatchListIcon;
--- src/contexts/moviesContext.js
+++ src/contexts/moviesContext.js
@@ -5,6 +5,7 @@ export const MoviesContext = React.createContext(null);
 const MoviesContextProvider = (props) => {
   const [myReviews, setMyReviews] = useState({});
   const [favorites, setFavorites] = useState([]);
+  const [mustWatch, setMustWatch] = useState([]);
 
   const addToFavorites = (movie) => {
     setFavorites([...favorites, movie.id]);
@@ -18,6 +19,11 @@ const MoviesContextProvider = (props) => {
     setMyReviews({ ...myReviews, [movie.id]: review });
   };
 
+  const addToMustWatch = (movie) => {
+    setMustWatch([...mustWatch, movie.id]);
+    console.log(mustWatch);
+  };
+
   return (
     <MoviesContext.Provider
       value={{
@@ -25,6 +31,7 @@ const MoviesContextProvider = (props) => {
         addToFavorites,
         removeFromFavorites,
         addReview,
+        addToMustWatch,
       }}
     >
       {props.children}

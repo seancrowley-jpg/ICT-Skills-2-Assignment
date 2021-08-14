698d4def8abb467def6d4006ed3c00d208cac9dc
Sean-Crowley <seancrowley033@gmail.com>
2021-08-14 15:24:28 +0100

    Added conditional for Pagaintion buttons to be removed from upcoming movies page

--- src/components/templateMovieListPage/index.js
+++ src/components/templateMovieListPage/index.js
@@ -36,7 +36,7 @@ function MovieListPageTemplate({ movies, title, action, page, setPage, isFetchin
       <Grid item xs={12}>
         <Header title={title} />
       </Grid>
-      <PaginationButtons page={page} setPage={setPage} isFetching={isFetching}/> 
+      <>{page ? <><PaginationButtons page={page} setPage={setPage} isFetching={isFetching}/></> : <></>}</>
       <Grid item container spacing={5}>
         <Grid key="find" item xs={12} sm={6} md={4} lg={3} xl={2}>
           <FilterCard
